19942efe737de3b990d4babb4e1d0f5cf321ff62
nishit <nishitmohanan@gmail.com>
2020-03-30 00:56:11 +0530

    Added additional stats and standardized country param
    Added critical,cases/deaths per million, 1st case date and recovery percentage. Also standardized the country parameter ref perv commit.

--- app/worldometer_scraper.py
+++ app/worldometer_scraper.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from countries import *
+import pdb
 
 data_pos = {
     0: "country_name",
@@ -9,12 +11,16 @@ data_pos = {
     4: "new_deaths",
     5: "total_recovered",
     6: "active_cases",
+    7: "critical",
+    8: "cases_per_million_pop",
+    9: "deaths_per_million_pop",
+    10: "1st_case_date",
 }
 
 
 def get_data():
     data = requests.get("https://www.worldometers.info/coronavirus/").text
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, "html.parser")
 
     table = soup.find("table", id="main_table_countries_today")
     rows = table.findAll("tr")
@@ -25,13 +31,47 @@ def get_data():
         country_data = {}
         for index, data in enumerate(cols):
             if data_pos.get(index):
-                country_data[data_pos[index]] = data.text.strip().strip("+")
-        if country_data.get('country_name'):
-            data_list[country_data['country_name'].lower()] = country_data
+                stat_number = data.text.strip().strip("+").replace(",", "")
+                if stat_number.isdigit():
+                    try:
+                        stat_number = int(stat_number)
+                    except ValueError as e:
+                        print(f"Error converting to int for data {data}")
+                else:
+                    stat_number = process_float(stat_number)
+
+                country_data[data_pos[index]] = stat_number
+
+        if country_data.get("country_name"):
+            if country_data["country_name"] == "Total:":
+                country_data["country_name"] = "World"
+            country_data["recovery_percentage"] = calculate_recovery_percentage(
+                total_cases=country_data["total_cases"],
+                recovered=country_data["total_recovered"],
+            )
+            try:
+                data_list[COUNTRY_LIST[country_data["country_name"]]] = country_data
+            except:
+                data_list[country_data["country_name"]] = country_data
+
     response["country_data"] = data_list
     return response
 
 
 if __name__ == "__main__":
     print(get_data())
+
+
+def process_float(value):
+    try:
+        return float(value)
+    except ValueError:
+        return value
+
+
+def calculate_recovery_percentage(total_cases, recovered):
+    try:
+        return round((recovered / total_cases) * 100, 2)
+    except:
+        return "NA"
 
