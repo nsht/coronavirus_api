7296e257f63cf54996b1cb7b3fcf226d20ec555b
nishit <nishitmohanan@gmail.com>
2020-04-01 23:29:41 +0530

    Modified OpenAPI documentation and error handling
    dded description and examples to the stats and country API, removed documentation for private APIs. Converted country name parameter in the API to upper case to fix unnecessary errors.

--- app/main.py
+++ app/main.py
@@ -18,24 +18,88 @@ app.add_middleware(
 )
 
 
-@app.get("/stats")
-def read_root():
+@app.get(
+    "/stats",
+    responses={
+        200: {
+            "description": "A list of dictionaries with all the stats",
+            "content": {
+                "application/json": {
+                    "example": """
+                    {
+                        "country_data": {
+                            "IN": {
+                                {
+                                    "country_name": "India",
+                                    "total_cases": 1637,
+                                    "new_cases": 240,
+                                    "total_deaths": 45,
+                                    "new_deaths": 10,
+                                    "total_recovered": 148,
+                                    "active_cases": 1444,
+                                    "critical": "",
+                                    "cases_per_million_pop": 1,
+                                    "deaths_per_million_pop": 0.03,
+                                    "1st_case_date": "Jan 29",
+                                    "recovery_percentage": 9.04,
+                                }
+                            }
+                        }
+                    }
+                    """
+                }
+            },
+        }
+    },
+)
+def get_stats():
+    """ Returns statistics for every country and a worldwide total"""
     return get_data()
 
 
-@app.get("/country/{country_name}")
-def get_items(country_name):
+@app.get(
+    "/country/{country_name}",
+    responses={
+        200: {
+            "description": "The statistics for the given country",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "country_name": "India",
+                        "total_cases": 1637,
+                        "new_cases": 240,
+                        "total_deaths": 45,
+                        "new_deaths": 10,
+                        "total_recovered": 148,
+                        "active_cases": 1444,
+                        "critical": "",
+                        "cases_per_million_pop": 1,
+                        "deaths_per_million_pop": 0.03,
+                        "1st_case_date": "Jan 29",
+                        "recovery_percentage": 9.04,
+                    }
+                }
+            },
+        }
+    },
+)
+def get_items(country_name: str):
+    """ Returns statistics for the specified country. The country name should be in the
+    3166-1 Alpha 2 format.
+    """
+
     return get_data()["country_data"].get(
-        country_name, {"status": False, "message": "Invalid Country name entered"}
+        country_name.upper(),
+        {"status": False, "message": "Invalid Country name entered"},
     )
 
 
-@app.get("/news/{country_name}")
-def get_items(country_name):
+@app.get("/news/{country_name}", include_in_schema=False)
+def get_items(country_name: str):
     return get_news(country_name=country_name)
 
 
-@app.get("/twitter_sentiment")
-def get_twitter_sentiment(location=None, search_term=None):
+@app.get("/twitter_sentiment", include_in_schema=False)
+def get_twitter_sentiment(location: str = None, search_term: str = None):
     print(search_term)
     return main(location, search_term)
