de8166b94d15c15b5acc7c749e244984b63268f2
nishit <nishitmohanan@gmail.com>
2020-04-03 20:30:17 +0530

    Added utility file to purge cache
    The file has a function purge_cache which can be manually called from the shell and run to clear the redis cache. Done to purge cache in case of changes in the data, such as country mapping change.

---
+++ app/admin_utils.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiocache import Cache
+
+
+def purge_cache(cache_key="all_stats"):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    cache = Cache(
+        Cache.REDIS,
+        endpoint="redis",
+        port=6379,
+        namespace="corona_api",
+        timeout=60 * 60 * 30,
+    )
+    loop.run_until_complete(cache.delete(cache_key))
+    print(f"Purged {cache_key}")
